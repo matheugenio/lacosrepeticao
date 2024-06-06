09027eee72dae59113706dedc6370d7289470aa5
Matheus Eugenio <math.eugenioti@gmail.com>
2024-06-06 10:45:46 -0300

    Inicialização do projeto e conclusão do primeiro exercício

---
+++ src/Exercicio1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número do intervalo: ");
+        int n1 = sc.nextInt();
+        System.out.println("Digite o segundo número do intervalo: ");
+        int n2 = sc.nextInt();
+
+        if(n1 > n2){
+            System.out.println("Intervalo inválido!");
+
+        } else {
+            for (int i=n1; i<=n2; i++) {
+                if (i%3==0 && i%5==0)
+                    System.out.println(i+" é múltiplo de 3 e 5");
+            }
+        }
+
+
+
+        sc.close();
+
+    }
+}
